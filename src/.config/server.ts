45a0da8493cfa521f705bbedff674db06f12dade
Mateus Felipe da Silveira Vieira <habdig7oficial@pop-os.localdomain>
2022-10-25 07:26:33 -0300

    teste

--- src/.config/server.ts
+++ src/.config/server.ts
@@ -4,6 +4,7 @@ import express from "express";
 import helmet from "helmet"; /* Lib pra proteger o site contra vunerabilidades conhecidas, recomendado pela documentação: https://expressjs.com/pt-br/advanced/best-practice-security.html */
 import compression from "compression"; /* Lib pra melhorar a performace com o gzip comprimindo dados, recomendado pela documentação: https://expressjs.com/pt-br/advanced/best-practice-performance.html*/
 import cookieParser from "cookie-parser"; /* Lib para trabalhar com cookies para aumentar a segurança */
+import cors from "cors"; /* Lib para trabalhar como api para requisições externas */
 
 /* Importando Rotas manuamente */
 import index from "../routes/test";
@@ -19,6 +20,10 @@ const port = process.env.PORT || 7777; /* Definindo a porta de execução */
 app.use(helmet()); /* Utilizando a lib do helmet */
 app.use(compression()); /* Utilizando a lib compression */
 app.use(cookieParser()); /* Utilizando a lib cookieParser */
+app.use(cors({
+  origin: "",
+  optionsSuccessStatus: 200
+}))
 
 app.set("views", "./src/EJS"); /* Alterando a pasta padrão views para a EJS */
 
