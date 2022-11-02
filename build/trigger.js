5970f7fdb056570bd929d2bc1b73610fd980204d
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2022-11-02 02:30:42 +0000

    Mateus Felipe da Silveira Vieira (habdig7oficial) - heroku stage v2

--- build/trigger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/** @format */
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./.config/server"); /* Importa configurações */
-server_1.app.listen(server_1.port, function () {
-    console.log(`✔ - Funcionando em http://0.0.0.0:${server_1.port}`);
-    /* Garante que o server está funcionando */
-});
